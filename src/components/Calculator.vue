50276f5d802c19d72f6d87f1fa6241a745a41f10
Isaac Curtiss <ijcurtiss7@gmail.com>
2019-05-10 18:23:25 -0700

    ability to type numbers and decimals properly

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -5,20 +5,20 @@
     <div @click="sign" class="btn">+/-</div>
     <div @click="percent" class="btn">%</div>
     <div class="btn operator">/</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
     <div class="btn operator">x</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')"  class="btn">6</div>
     <div class="btn operator">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
     <div class="btn operator">+</div>
-    <div class="btn zero">0</div>
-    <div class="btn">.</div>
+    <div @click="append('0')" class="btn zero">0</div>
+    <div @click="dot()" class="btn">.</div>
     <div class="btn operator">=</div>
   </div>
 </template>
@@ -27,7 +27,7 @@
 export default {
   data() {
     return {
-      current: '600',
+      current: '',
     }
   },
   methods: {
@@ -39,6 +39,14 @@ export default {
     },
     percent() {
       this.current = `${ parseFloat(this.current) / 100 }`;
+    },
+    append(number) {
+      this.current = `${ this.current }${ number }`;
+    },
+    dot() {
+      if(this.current.indexOf('.') === -1 ) {
+        this.append('.');
+      }
     }
   }
 
