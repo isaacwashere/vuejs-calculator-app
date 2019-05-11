fad4c6f9d2dc289318c245044cbd5fef0762f27b
Isaac Curtiss <ijcurtiss7@gmail.com>
2019-05-10 19:10:43 -0700

    ability to use calculator for opertions

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="calculator">
-    <div class="display">{{ current || 0 }}</div>
+    <div class="display">{{ currentNum || 0 }}</div>
     <div @click="clear" class="btn">C</div>
     <div @click="sign" class="btn">+/-</div>
     <div @click="percent" class="btn">%</div>
-    <div class="btn operator">/</div>
+    <div @click="divide" class="btn operator">/</div>
     <div @click="append('7')" class="btn">7</div>
     <div @click="append('8')" class="btn">8</div>
     <div @click="append('9')" class="btn">9</div>
-    <div class="btn operator">x</div>
+    <div @click="multiply" class="btn operator">x</div>
     <div @click="append('4')" class="btn">4</div>
     <div @click="append('5')" class="btn">5</div>
     <div @click="append('6')"  class="btn">6</div>
-    <div class="btn operator">-</div>
+    <div @click="subtract" class="btn operator">-</div>
     <div @click="append('1')" class="btn">1</div>
     <div @click="append('2')" class="btn">2</div>
     <div @click="append('3')" class="btn">3</div>
-    <div class="btn operator">+</div>
+    <div @click="add" class="btn operator">+</div>
     <div @click="append('0')" class="btn zero">0</div>
     <div @click="dot()" class="btn">.</div>
-    <div class="btn operator">=</div>
+    <div @click="equal()" class="btn operator">=</div>
   </div>
 </template>
 
@@ -27,33 +27,65 @@
 export default {
   data() {
     return {
-      current: '',
+      previousNum: null,
+      currentNum: '',
+      operator: null,
+      operatorClicked: false
     }
   },
   methods: {
     clear() {
-      this.current = '';
+      this.currentNum = '';
     },
     sign() {
-      this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : `-${ this.current }`;
+      this.currentNum = this.currentNum.charAt(0) === '-' ? this.currentNum.slice(1) : `-${ this.currentNum }`;
     },
     percent() {
-      this.current = `${ parseFloat(this.current) / 100 }`;
+      this.currentNum = `${ parseFloat(this.currentNum) / 100 }`;
     },
     append(number) {
-      this.current = `${ this.current }${ number }`;
+      if (this.operatorClicked) {
+        this.currentNum = '';
+        this.operatorClicked = false;
+      }
+      this.currentNum = `${ this.currentNum }${ number }`;
     },
     dot() {
-      if(this.current.indexOf('.') === -1 ) {
+      if (this.currentNum.indexOf('.') === -1 ) {
         this.append('.');
       }
+    },
+    setPreviousNum() {
+      this.previousNum = this.currentNum;
+      this.operatorClicked = true;
+    },
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.setPreviousNum();
+    },
+    multiply() {
+      this.operator = (a, b) => a * b;
+      this.setPreviousNum();
+    },
+    subtract() {
+      this.operator = (a, b) => a - b;
+      this.setPreviousNum();
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.setPreviousNum();
+    },
+    equal() {
+      this.currentNum = `${this.operator(
+        parseFloat(this.previousNum), 
+        parseFloat(this.currentNum)
+      )}`;
+      this.previousNum = null;
     }
   }
-
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .calculator {
     margin: 0 auto;
